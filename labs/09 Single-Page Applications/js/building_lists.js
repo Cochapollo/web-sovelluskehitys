15b3dba2bd48ddefd57c7caa592499ce3a5eac74
petrifo <petriforsman@metropolia.fi>
2019-11-14 14:02:31 +0200

    helloed

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -7,11 +7,24 @@ console.log(data);
 
 var books = data.books;
 
-var list = document.createElement('ol');
+var list = document.createElement('table');
+var headings = document.createElement('tr');
+var title1 = document.createElement('th');
+var year1 = document.createElement('th');
+title1.innerHTML = "Title";
+year1.innerHTML = "Year of publication";
+headings.appendChild(title1);
+headings.appendChild(year1);
+list.appendChild(headings)
 for (var i=0; i < books.length; i++) {
 	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
+	var item = document.createElement('tr');
+	var title = document.createElement('td');
+	var year = document.createElement('td');
+	item.appendChild(title);
+	item.appendChild(year);
+	title.innerHTML = books[i].title;
+	year.innerHTML = books[i].year;
 	list.appendChild(item);
 }
 document.body.appendChild(list);
--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -17,11 +17,32 @@ document.getElementById('save').onclick = function () {
 	paragraphs[1].innerHTML = 'Hello World!';
 };
 document.querySelector('#userForm input[type="email"]').onkeyup = function() {
+	var name = document.querySelector('#userForm input[type="text"]').value;
+
+	var password = document.querySelector('#userForm input[type="password"]').value;
 	console.log('updating email');
 	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
+	document.querySelector('#summary p').innerHTML = email+" "+name+" "+password;
+	document.querySelector('#summary h1').innerHTML = name+ " " +email+" "+password;
 };
+document.querySelector('#userForm input[type="password"]').onkeyup = function () {
+	var name = document.querySelector('#userForm input[type="text"]').value;
 
+	var password = document.querySelector('#userForm input[type="password"]').value;
+	console.log('updating email');
+	var email = document.querySelector('#userForm input[type="email"]').value;
+	document.querySelector('#summary p').innerHTML = email+" "+name+" "+password;
+	document.querySelector('#summary h1').innerHTML = name+ " " +email+" "+password;
+};
+document.querySelector('#userForm input[type="text"]').onkeyup = function () {
+	var name = document.querySelector('#userForm input[type="text"]').value;
+
+	var password = document.querySelector('#userForm input[type="password"]').value;
+
+	var email = document.querySelector('#userForm input[type="email"]').value;
+	document.querySelector('#summary p').innerHTML = email+" "+name+" "+password;
+	document.querySelector('#summary h1').innerHTML = name+ " " +email+" "+password;
+};
 /*
 function save() {
 	console.log('save');
